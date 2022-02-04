f7e6c9412ebe03b98bf6a5902cd1917a22bcf08e
Tom Camp <tom.camp@civicactions.com>
2022-02-04 14:17:55 -0500

    Adding new download page.

--- utils.py
+++ utils.py
@@ -1,20 +1,21 @@
 def project_navigation(request, project):
-  task = project.root_task.get_or_create_subtask(request.user, "ssp_intro")
+  task = project.root_task.get_or_create_subtask(request.user, 'ssp_intro')
+  project_url = project.get_absolute_url()
   nav = {
-      "controls": {
-          "url": "/systems/" + str(project.system.id) + "/controls/selected",
-          "title": "Review Controls",
-          "id": "project-controls",
+      'controls': {
+          'url': f'/systems/{project.system.id}/controls/selected',
+          'title': 'Review Controls',
+          'id': 'project-controls',
       },
-      "components": {
-          "url": "/systems/" + str(project.system.id) + "/components/selected",
-          "title": "Manage Components",
-          "id": "project-components",
+      'components': {
+          'url': f'/systems/{project.system.id}/components/selected',
+          'title': 'Manage Components',
+          'id': 'project-components',
       },
-      "ssp": {
-          "url": task.get_absolute_url(),
-          "title": "Export System Security Plan",
-          "id": "project-ssp"
+      'ssp': {
+          'url': f'{project_url}/downloads',
+          'title': 'Export System Security Plan',
+          'id': 'project-ssp',
       },
   }
   return nav
