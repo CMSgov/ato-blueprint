32cf0031352fc05c0a48fabbf5220ca0273604a8
Amy DiPasquale <adipasquale@fearless.tech>
2022-03-01 18:20:37 -0500

    project integrations continued

--- project/views.py
+++ project/views.py
@@ -2,26 +2,28 @@ from django.shortcuts import redirect, render
 from .models import Package
 from .forms import PackageForm
 
-from utils import project_navigation
+from utils import package_navigation
 
 def project(request, project_id):
     project = Package.objects.get(id=project_id)
 
-    return render(request, "project.html", {
+    return render(request, "project/project-home.html", {
         "project": project,
-        "security_sensitivity": project.impact_level,
+        "acronym": project.acronym,
+        "project_title": project.title,
+        "impact_level": project.impact_level,
         # "controls_status_count": project.system.controls_status_count,
         # "poam_status_count": project.system.poam_status_count,
         # "is_admin": request.user in project.get_admins(),
-        # "title": project.title,
         # "columns": columns,
         # "elements": elements,
         # "total_controls_count": control_compliance_stats.get('total_controls_count'),
         # "controls_addressed_count": control_compliance_stats.get('controls_addressed_count'),
         # "percent_compliant": control_compliance_stats.get('percent_compliant'),
-        # "nav": project_navigation(request, project),
+        "nav": package_navigation(project),
     })
 
+# Example def that is not yet functional
 def project_component_detail(request, project_id, component_id):
     project = Package.objects.get(id=project_id)
 
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+# TODO this is part of the old project related code and will be replaced by the "package_navigation" function below.
+# This function can be removed when no longer in use.
 def project_navigation(request, project):
   task = project.root_task.get_or_create_subtask(request.user, 'ssp_intro')
   project_url = project.get_absolute_url()
@@ -14,6 +16,28 @@ def project_navigation(request, project):
       },
       'ssp': {
           'url': f'{project_url}/downloads',
+          'title': 'Export System Security Plan',
+          'id': 'project-ssp',
+      },
+  }
+  return nav
+
+# TODO rename package_navigation to project_navigation once old project related code is removed
+def package_navigation(project):
+  project_url = project.get_absolute_url()
+  nav = {
+      'controls': {
+          'url': f'{project_url}/controls',
+          'title': 'Review Controls',
+          'id': 'project-controls',
+      },
+      'components': {
+          'url': f'{project_url}/components',
+          'title': 'Manage Components',
+          'id': 'project-components',
+      },
+      'ssp': {
+          'url': f'{project_url}/download',
           'title': 'Export System Security Plan',
           'id': 'project-ssp',
       },
