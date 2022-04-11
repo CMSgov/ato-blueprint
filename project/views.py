a765e0aee758f81cf2dfbdc74bbd7c5d6b7871e5
amyd11 <adipasquale@fearless.tech>
2022-04-11 15:38:45 -0400

    Feature/new projects page (#87)
    * WIP new projects page
    
    * add tests for projects page
    
    * project cards cleanup
    
    * remove placeholder comments in the project view

--- project/views.py
+++ project/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
 
 from utils import package_navigation
 
@@ -7,34 +9,53 @@ from .forms import PackageForm
 from .models import Package
 
 
+@method_decorator(login_required, name="dispatch")
+class PackageList(ListView):
+    model = Package
+    template_name = "packages.html"
+    context_object_name = "projects"
+    ordering = ["created"]
+    paginate_by = 9
+
+    def get_queryset(self):
+        projects = Package.objects.filter(creator=self.request.user.id)
+        return list(projects)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
 @login_required
 def project(request, project_id):
     project = Package.objects.get(id=project_id)
 
-    return render(request, "project/project-home.html", {
-        "project": project,
-        "acronym": project.acronym,
-        "project_title": project.title,
-        "impact_level": project.impact_level,
-        # "controls_status_count": project.system.controls_status_count,
-        # "poam_status_count": project.system.poam_status_count,
-        # "is_admin": request.user in project.get_admins(),
-        # "columns": columns,
-        # "elements": elements,
-        # "total_controls_count": control_compliance_stats.get('total_controls_count'),
-        # "controls_addressed_count": control_compliance_stats.get('controls_addressed_count'),
-        # "percent_compliant": control_compliance_stats.get('percent_compliant'),
-        "nav": package_navigation(project),
-    })
+    return render(
+        request,
+        "project/project-home.html",
+        {
+            "project": project,
+            "acronym": project.acronym,
+            "project_title": project.title,
+            "impact_level": project.impact_level,
+            "nav": package_navigation(project),
+        },
+    )
+
 
 # TODO Example def that is not yet functional
 @login_required
 def project_component_detail(request, project_id, component_id):
     project = Package.objects.get(id=project_id)
 
-    return render(request, "project.html", {
-        "project": project,
-    })
+    return render(
+        request,
+        "project.html",
+        {
+            "project": project,
+        },
+    )
+
 
 @login_required
 def create_project(request):
@@ -49,7 +70,7 @@ def create_project(request):
             new_project.creator_id = request.user.id
             new_project.save()
 
-            return redirect('projects')
+            return redirect("projects")
 
     # If this is a GET (or any other method) create the default form
     else:
